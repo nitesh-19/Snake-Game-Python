50d6d2cd10c4b68a82761ec352a1047a8ad4752c
nitesh-19 <niteshgohil25@gmail.com>
2023-08-18 12:30:55 +0530

    Added methods to move snake following key presses

--- snake_game.py
+++ snake_game.py
@@ -10,39 +10,55 @@ segments = []
 
 
 def up():
-    segments[0].setheading(90)
-    segments[0].forward(20)
-    move_body(segments)
-    screen.update()
+
+    if segments[0].heading() == 270:
+        pass
+    else:
+        segments[0].setheading(90)
+
+
+def left():
+    segments[0].setheading(180)
+
+
+def right():
+    segments[0].setheading(0)
+
+
+def down():
+    segments[0].setheading(270)
 
 
 def move_body():
-    for i in range(1, len(segments) - 1):
-        x_pos = segments[-i - 1].xcor()
-        y_pos = segments[-i - 1].ycor()
-        segments[-i].goto(x_pos, y_pos)
-        segments[0].forward(20)
+    prev_coordinates = []
+    for obj in segments:
+        prev_coordinates.append((obj.xcor(), obj.ycor()))
+    segments[0].forward(20)
+    for i in range(0, len(segments) - 1):
+        segments[i + 1].goto(prev_coordinates[i])
+        # screen.update()
 
 
-for _ in range(0, 3):
+number_of_turtles = 5
+for _ in range(0, number_of_turtles):
     new_turtle = Turtle(shape="square")
     new_turtle.color("white")
     new_turtle.penup()
-    # new_turtle.speed("slowest")
     segments.append(new_turtle)
+segments[1].color("green")
+segments[2].color("blue")
 
 for index in range(0, len(segments)):
     segments[index].setpos((-index * 20, 0))
-screen.update()
-game_is_on = True
-last_position = []
-screen.listen()
-while game_is_on:
-    # screen.onkeypress(fun=up, key="Up")
-
-    move_body()
-    # time.sleep(0.08)
-
     screen.update()
-
+game_on = True
+screen.listen()
+while game_on:
+    move_body()
+    screen.onkeypress(fun=up, key="Up")
+    screen.onkeypress(fun=left, key="Left")
+    screen.onkeypress(fun=right, key="Right")
+    screen.onkeypress(fun=down, key="Down")
+    time.sleep(0.08)
+    screen.update()
 screen.exitonclick()
