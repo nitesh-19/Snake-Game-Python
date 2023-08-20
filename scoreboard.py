65f246b1cee73d508ea16b20d91204fbd36faae6
nitesh-19 <niteshgohil25@gmail.com>
2023-08-20 19:49:14 +0530

    Added highscore tracking

--- scoreboard.py
+++ scoreboard.py
@@ -8,7 +8,8 @@ class Scoreboard(Turtle):
         self.penup()
         self.color("white")
         self.hideturtle()
-
+        with open("data.txt", "r") as file:
+            self.highscore = int(file.read())
         self.goto(0, 280)
         self.refresh()
 
@@ -16,8 +17,12 @@ class Scoreboard(Turtle):
         self.clear()
         self.score += 1
         self.write(f"Score: {self.score}", align="center")
+        if self.score > self.highscore:
+            self.highscore = self.score
         self.goto(0, 280)
 
     def game_over(self):
         self.goto(0, 0)
         self.write("GAME OVER", align="center")
+        with open("data.txt", "w") as file:
+            file.write(str(self.highscore))
